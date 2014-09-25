80267cf8a5fa140016a55ece655e57f825fbf551
Horacio Fernandes <horacioalexandre.fernandes@gmail.com>
2014-09-25 08:12:58 -0300

    finalized concat values of two arrays

--- day2/easy.lua
+++ day2/easy.lua
@@ -1,11 +1,22 @@
 function concate_tables(table1, table2)
 	for k, v in pairs(table2) do
-		
+		table1[#table1 + 1] = v
 	end
+	return table1
 end 
+
+function print_table(t)
+	for k, v in pairs(t) do
+		print(k..':'..v)
+	end
+end
+
 
 table1 = {'horacio', 'marcio', 'tiago'}
 table2 = {26, 26, 21}
 
+
 result = concate_tables(table1, table2)
-print(result)
+print_table(result)
+
+
